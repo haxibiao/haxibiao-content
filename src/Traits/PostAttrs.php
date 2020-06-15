07f6afde65fe76c11e7cdd920322f8e891ee16ed
龚国玮 <gongguowei@haxifang.com>
2020-06-15 22:01:58 +0800

    fix:视频列表接口崩溃

--- src/Traits/PostAttrs.php
+++ src/Traits/PostAttrs.php
@@ -6,7 +6,7 @@ use Carbon\Carbon;
 
 trait PostAttrs
 {
-    public function getTimeAgoAttribute()
+    public function getTimeAgeAttribute()
     {
         return time_ago($this->created_at);
     }
@@ -17,16 +17,5 @@ trait PostAttrs
             return $user->isLiked('posts', $this->id);
         }
         return false;
-    }
-
-    public function getCreatedTimeAttribute()
-    {
-        //时间差，视频发布时间与今天的时间差
-        (string)$timeDifference = Carbon::parse($this->created_at)->diffInDays(today());
-
-        if (0 == $timeDifference) {
-            return "今天";
-        }
-        return $timeDifference . '天前';
     }
 }
