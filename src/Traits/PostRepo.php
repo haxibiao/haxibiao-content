5c08df2979b1a915c68b7bced4ce69242dc063c7
吴秀璋 <wuxiuzhang@haxibiao.com>
2020-06-16 16:10:28 +0800

    fixed:查询用户动态报错

--- src/Traits/PostRepo.php
+++ src/Traits/PostRepo.php
@@ -442,7 +442,7 @@ trait PostRepo
     //个人主页动态
     public static function posts($user_id)
     {
-        Post::latest('id')->publish()->where('user_id', $user_id);
+        return Post::latest('id')->publish()->where('user_id', $user_id);
     }
 
     //分享post链接
