4269e483c0229c9026cc1cbf5f5837bf2e054c33
ivan <ivan@haxibiao.com>
2020-06-19 17:57:17 +0800

    更新matomo的 category

--- src/Traits/PostResolvers.php
+++ src/Traits/PostResolvers.php
@@ -3,19 +3,18 @@
 namespace haxibiao\content\Traits;
 
 use haxibiao\content\Post;
-use App\Exceptions\GQLException;
 
 trait PostResolvers
 {
     public function resolveRecommendPosts($root, $args, $context, $info)
     {
-        app_track_user_event("获取学习视频");
+        app_track_event("首页", "获取学习视频");
         return Post::getRecommendPosts();
     }
 
     public function resolvePosts($root, $args, $context, $info)
     {
-        app_track_user_event("个人主页视频动态");
+        app_track_event("用户页", "视频动态");
 
         return Post::posts($args['user_id']);
     }
@@ -25,7 +24,7 @@ trait PostResolvers
      */
     public function resolvePublicPosts($root, $args, $context, $info)
     {
-        app_track_user_event("访问动态广场");
+        app_track_event("首页", "访问动态广场");
         return Post::PublicPosts($args['user_id'] ?? null);
     }
 
@@ -34,7 +33,7 @@ trait PostResolvers
      */
     public function getShareLink($rootValue, array $args, $context, $resolveInfo)
     {
-        app_track_user('分享视频');
+        app_track_user('分享', '分享视频');
         return Post::shareLink($args['id']);
 
         $qb = Post::latest('id');
