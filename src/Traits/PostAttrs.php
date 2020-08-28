ab9b554fa0b324a92acc5db1dc4aaffca1c96cca
唐淑娟 <tangshujuan@haxifang.cn>
2020-08-28 10:02:42 +0800

    Revert "DZ-1699 fixed:修复post.liked没有采用预加载属性,导致重复查询"错误提交回滚
    This reverts commit 47f8a100dfd1bb6f65686e5981eaf02f061b9f5c.

--- src/Traits/PostAttrs.php
+++ src/Traits/PostAttrs.php
@@ -15,13 +15,9 @@ trait PostAttrs
     }
     public function getLikedAttribute()
     {
-        //检查下是否预加载了,预加载后,则无需重复查询
-        $isPredloaded = isset($this->attributes['liked']);
-        $liked        = $isPredloaded ? $this->attributes['liked'] : false;
-        if (!$isPredloaded && $user = checkUser()) {
-            $liked = $user->isLiked('posts', $this->id);
+        if ($user = checkUser()) {
+            return $user->isLiked('posts', $this->id);
         }
-
-        return $liked;
+        return false;
     }
 }
