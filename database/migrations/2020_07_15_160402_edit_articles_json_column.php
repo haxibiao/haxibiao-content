ab8c554e10788587a1542a935d1a5ee5a2a3ab86
张志明 <zhangzhiming@haxifang.com>
2020-09-03 17:27:14 +0800

    修复class重复命名

--- database/migrations/2020_07_15_160402_edit_articles_json_column.php
+++ database/migrations/2020_07_15_160402_edit_articles_json_column.php
@@ -4,8 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateArticlesTable extends Migration
+class EditArticlesJsonColumn extends Migration
 {
+
     /**
      * Run the migrations.
      *
