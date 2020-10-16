22ab684c7968f35c3a1a0aae51657f81b1b2dc8e
唐淑娟 <tangshujuan@haxifang.cn>
2020-10-16 14:40:07 +0800

    添加合集展示限制

--- src/Traits/CollectionResolvers.php
+++ src/Traits/CollectionResolvers.php
@@ -202,6 +202,8 @@ trait CollectionResolvers
                 $qb = $qb->whereNotIn('id', $visitVideoIds);
             }
         }
+        //动态数量大于三的
+        $qb = $qb->where('count_posts','>=',3);
         //随机进行排序 最近七天的
         $qb = $qb->inRandomOrder()
             ->whereBetWeen('created_at', [today()->subDay(7), today()]);
