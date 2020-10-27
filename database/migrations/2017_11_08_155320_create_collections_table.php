f72e4bcf8579db0e284ee82663243c9a3c3c50b5
xxm <xiaoxinming@haxibiao.com>
2020-10-27 21:00:40 +0800

    fixed:修复collections migrate 造成的table  already exists

--- database/migrations/2017_11_08_155320_create_collections_table.php
+++ database/migrations/2017_11_08_155320_create_collections_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateCollectionsTable extends Migration
 {
@@ -13,28 +13,30 @@ class CreateCollectionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('collections', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('user_id')->index();
-            $table->integer('status')->default(1)->index();  // 0 private 1 public
-            $table->string('type')->default('article')->index(); // faved...
-            $table->string('name');
-            $table->string('description');
-            $table->string('logo')->nullable();
-            $table->json('json')->nullable()->comment('非结构化的数据，冗余一些额外信息');
-            //集合排序字段
-            $table->unsignedInteger('sort_rank')->nullable()->index()->comment('排序(置顶方法)');
+        if (!Schema::hasTable('collections')) {
+            Schema::create('collections', function (Blueprint $table) {
+                $table->increments('id');
+                $table->integer('user_id')->index();
+                $table->integer('status')->default(1)->index(); // 0 private 1 public
+                $table->string('type')->default('article')->index(); // faved...
+                $table->string('name');
+                $table->string('description');
+                $table->string('logo')->nullable();
+                $table->json('json')->nullable()->comment('非结构化的数据，冗余一些额外信息');
+                //集合排序字段
+                $table->unsignedInteger('sort_rank')->nullable()->index()->comment('排序(置顶方法)');
 
-            //add counts
-            $table->integer('count')->default(0);
-            $table->integer('count_words')->default(0);
-            $table->integer('count_follows')->default(0)->index();
-            $table->unsignedInteger('count_posts')->default(0)->index();
-            $table->unsignedInteger('count_views')->default(0)->comment('浏览量');
+                //add counts
+                $table->integer('count')->default(0);
+                $table->integer('count_words')->default(0);
+                $table->integer('count_follows')->default(0)->index();
+                $table->unsignedInteger('count_posts')->default(0)->index();
+                $table->unsignedInteger('count_views')->default(0)->comment('浏览量');
 
-            $table->timestamps();
-            $table->softDeletes();
-        });
+                $table->timestamps();
+                $table->softDeletes();
+            });
+        }
     }
 
     /**
