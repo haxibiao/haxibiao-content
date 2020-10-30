11f7a56cbc76386da8d2ccc5735e6d3e8e02af2f
唐淑娟 <tangshujuan@haxifang.cn>
2020-10-30 16:56:23 +0800

    add import:collection comand

--- config/haxibiao-content.php
+++ config/haxibiao-content.php
@@ -38,4 +38,7 @@ return [
     'enabled_video_share'  => env('ENABLED_VIDEO_SHARE',false),
     // 超过这个大小的视频不参与视频分享 100M=50*1024*1024
     'video_threshold_size' => env('VIDEO_THRESHOLD_SIZE',100*1024*1024),
+     //数据源的 cos domian
+     'origin_cos_domain' => env('ORIGIN_COS_DOMAIN', ''),
+
 ];
