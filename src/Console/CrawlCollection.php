f53302491649982da585214809493931c1e0418d
唐淑娟 <tangshujuan@haxifang.cn>
2020-11-05 16:51:10 +0800

    add CrawlCollection Command

---
+++ src/Console/CrawlCollection.php
@@ -0,0 +1,259 @@
+<?php
+
+namespace Haxibiao\Content\Console;
+
+use App\Post;
+use App\User;
+use App\Spider;
+use App\Collection;
+use GuzzleHttp\Client;
+use Illuminate\Console\Command;
+use App\Exceptions\GQLException;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+
+class CrawlCollection extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'crawl:collection';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '按用户主页合集爬取视频';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    const COLLECTIONS_URL = "https://aweme-lq.snssdk.com/aweme/v1/mix/list/?user_id=%s&cursor=%s&count=%s";
+    const VIDEOS_URL = "https://aweme-lq.snssdk.com/aweme/v1/mix/aweme/?mix_id=%s&cursor=%s&count=%s";
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        //获取需要爬取的信息
+        $sourceData = self::getJsonData('file/collections.json');
+        foreach ($sourceData as $userPage) {
+            $share_link = data_get($userPage, 'share_link');
+
+            $collectionsName = data_get($userPage, 'collections');
+            $userName = data_get($userPage, 'vest_user');
+            $this->info("start crawl user " . $share_link);
+            self::processCrawl($share_link, $collectionsName, $userName);
+        }
+
+        return 0;
+    }
+
+    public static function processCrawl($share_link, $collectionsName, $userName)
+    {
+
+        //获取抖音用户id
+        $user_id = self::getDouYinUserId($share_link);
+        $vestUser = User::where('name', $userName)->first();
+        if (empty($vestUser)) {
+            $vestUser = User::where('role_id', User::VEST_STATUS)->inRandomOrder()->first();
+        }
+
+        $hasMore = true;
+        for ($cursor = 0, $count = 15; $hasMore; $cursor++) {
+            $crawlUrl = sprintf(self::COLLECTIONS_URL, $user_id, $cursor, $count);
+            //获取用户所有的合集信息
+            $collectionData = self::getRequestData($crawlUrl);
+            $hasMore = (bool) data_get($collectionData, 'has_more', 0);
+            $mixInfos = data_get($collectionData, 'mix_infos', null);
+            throw_if(empty($mixInfos), GQLException::class, '未找到该用户的合集!');
+
+            $collections = [];
+            $mixIds = [];
+
+            //未指定爬取合集的情况，爬取该用户下的所有合集
+            if (empty($collectionsName)) {
+                foreach ($mixInfos as $mixInfo) {
+                    info("开始爬取合集 " . data_get($mixInfo, 'mix_name'));
+                    //创建对应的collection
+                    $collection = self::getMixInfoCollection($mixInfo, $vestUser);
+                    $mixId = data_get($mixInfo, 'mix_id');
+                    $mixIds[] = $mixId;
+                    $collections[$mixId] = $collection;
+                }
+
+            } else {
+                //爬取指定的合集
+                foreach ($mixInfos as $mixInfo) {
+                    $name = data_get($mixInfo, 'mix_name');
+                    if (in_array($name, $collectionsName)) {
+                        info("开始爬取合集 " . $name);
+                        //创建对应的collection
+                        $collection = self::getMixInfoCollection($mixInfo, $vestUser);
+                        $mixId = data_get($mixInfo, 'mix_id');
+                        $mixIds[] = $mixId;
+                        $collections[$mixId] = $collection;
+                    } else {
+                        continue;
+                    }
+
+                }
+            }
+
+        }
+
+        //爬取每个合集下的视频
+        foreach ($mixIds as $mixId) {
+            $hasMore = true;
+            $postIds = [];
+            for ($cursor = 0, $count = 15; $hasMore; $cursor++) {
+                $crawlUrl = sprintf(self::VIDEOS_URL, $mixId, $cursor, $count);
+                $videoData = self::getRequestData($crawlUrl);
+                $hasMore = (bool) data_get($videoData, 'has_more', 0);
+
+                $videos = data_get($videoData, 'aweme_list');
+                foreach ($videos as $video) {
+                    info("开始爬取视频 " . data_get($video, 'desc'));
+                    $created = date('Y-m-d H:i:s', data_get($video, 'create_time'));
+                    $shareUrl = data_get($video, 'share_info.share_url');
+                    //提前创建对应的爬虫对象
+                    $spider = Spider::firstOrNew(['source_url' => $shareUrl]);
+                    $spider->user_id = $vestUser->id;
+                    $spider->spider_type = 'videos';
+                    $spider->saveDataOnly();
+                    //创建对应的动态
+                    $post = Post::firstOrNew(['spider_id' => $spider->id]);
+                    $post->status = Post::PRIVARY_STATUS;
+                    $post->created_at = $created;
+                    $post->user_id = $vestUser->id;
+                    $reviewDay = $post->created_at->format('Ymd');
+                    $reviewId = Post::makeNewReviewId($reviewDay);
+                    $post->review_id = $reviewId;
+                    $post->review_day = $reviewDay;
+                    $post->save();
+                    $postIds[] = $post->id;
+
+                    //登录
+                    Auth::login($vestUser);
+                    try {
+                        //爬取对应的
+                        $spider->resolveShareLink(null, ['share_link' => $shareUrl], null, null);
+                    } catch (\Exception $ex) {
+                        $info=$ex->getMessage();
+                        if(!$info=="正在重新采集中,请稍后再看!"){
+                            info("异常信息".$info);
+                            throw $ex;
+                        }
+                    }
+
+                }
+            }
+
+            //将视频归入合集中
+            $collection = $collections[$mixId];
+            $collection->posts()->syncWithoutDetaching($postIds);
+            $collection->updateCountPosts();
+            info($collection->name."爬取完毕");
+        }
+
+    }
+
+    //根据mix_info创建Collection
+    public static function getMixInfoCollection($mixInfo, $vestUser)
+    {
+        $name = data_get($mixInfo, 'mix_name');
+        $description = data_get($mixInfo, 'desc');
+        $logo = data_get($mixInfo, 'cover_url.url_list.0');
+
+        $cosPath = 'images/' . uniqid() . '.jpeg';
+        Storage::cloud()->put($cosPath, file_get_contents($logo));
+        $newImagePath = Storage::cloud()->url($cosPath);
+        $collection = Collection::firstOrCreate(
+            ['name' => $name, 'user_id' => $vestUser->id],
+            [
+                'description' => $description,
+                'type' => 'posts',
+                'logo' => $newImagePath,
+                'status' => Collection::STATUS_ONLINE,
+                'json' => [
+                    'mix_info' => $mixInfo,
+                ]]
+        );
+        
+        return $collection;
+    }
+
+    //解析json数据
+    public static function getJsonData($path = 'file/collections.json')
+    {
+
+        $completePath = Storage::cloud()->url($path);
+        // 从文件中读取数据到PHP变量
+        $json_string = file_get_contents($completePath);
+
+        // 用参数true把JSON字符串强制转成PHP数组
+        $data = json_decode($json_string, true);
+        return $data;
+
+    }
+
+    //获取抖音用户id
+    public static function getDouYinUserId($url = '')
+    {
+
+        throw_if(is_null($url), GQLException::class, "主页链接为空");
+
+        $url = Spider::extractURL($url);
+
+        $client = new Client();
+        $response = $client->request('GET', $url, [
+            'http_errors' => false,
+            'allow_redirects' => false,
+        ]);
+        throw_if($response->getStatusCode() == 404, GQLException::class, '您分享的链接不存在,请稍后再试!');
+
+        $referUrl = data_get($response->getHeader('location'), '0', '');
+        throw_if(is_null($referUrl), GQLException::class, "获取抖音用户ID失败");
+
+        $start = strripos($referUrl, "/");
+        $end = strpos($referUrl, "?");
+
+        $user_id = substr($referUrl, $start + 1, $end - $start - 1);
+
+        return $user_id;
+    }
+
+    /**
+     *  获取链接数据
+     * $url:需要请求的分页地址
+     */
+
+    public static function getRequestData($url = '')
+    {
+        $client = new Client();
+        $response = $client->request('GET', $url, [
+            'http_errors' => false,
+        ]);
+        throw_if($response->getStatusCode() == 404, GQLException::class, '您分享的链接不存在,请稍后再试!');
+
+        $contents = $response->getBody()->getContents();
+        throw_if(empty($contents), GQLException::class, '获取内容链接失败!');
+
+        $contents = json_decode($contents, true);
+        return $contents;
+
+    }
+}
