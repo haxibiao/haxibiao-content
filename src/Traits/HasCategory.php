c30ce0087a63ca4a8ef9f10b8601a62f9b5648d1
杨倩 <yangqian@haxifang.cn>
2020-12-04 18:23:20 +0800

    NHDY-240 fixed 我的专题管理下没有数据

--- src/Traits/HasCategory.php
+++ src/Traits/HasCategory.php
@@ -35,6 +35,11 @@ trait HasCategory
         return $this->hasMany($this->categorizableModel(), 'user_id', 'id')->where('type', 'article');
     }
 
+    public function hasCategories()
+    {
+        return $this->hasMany($this->categorizableModel(), 'user_id', 'id')->where('type', 'diagrams')->where('status','1');
+    }
+
     public function newReuqestCategories()
     {
         return $this->adminCategories()->orderBy('new_requests', 'desc')->orderBy('updated_at', 'desc');
