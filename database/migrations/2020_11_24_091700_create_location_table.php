fd6a52b699eb9d92e1ddc50345f263a5f3070f1c
吴秀璋 <wuxiuzhang@haxibiao.com>
2020-12-15 10:10:05 +0800

    fix:migration locations error

--- database/migrations/2020_11_24_091700_create_location_table.php
+++ database/migrations/2020_11_24_091700_create_location_table.php
@@ -13,17 +13,20 @@ class CreateLocationTable extends Migration
      */
     public function up()
     {
+        if (Schema::hasTable('locations')) {
+            return;
+        }
         Schema::create('locations', function (Blueprint $table) {
             $table->id();
             $table->morphs('located');
             $table->string('address')->comment('详细地址');
             $table->string('description')->comment('大概地址描述');
             $table->string('district')->comment('区域,统计层数据用');
-            $table->double('longitude',9,6)->comment('精度');
-            $table->double('latitude',8,6)->comment('精度');
+            $table->double('longitude', 9, 6)->comment('精度');
+            $table->double('latitude', 8, 6)->comment('精度');
             $table->string('geo_code')->index()->comment('geohash code');
 
-            $table->index(['longitude','latitude'],'lon_lat');
+            $table->index(['longitude', 'latitude'], 'lon_lat');
             $table->timestamps();
         });
     }
