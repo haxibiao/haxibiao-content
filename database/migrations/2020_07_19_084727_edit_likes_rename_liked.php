07a3421278a4865570eb10877c912ebac51e85aa
ivan <ivan@mac>
2020-12-31 16:38:36 +0800

    兼容dianmoge等半重构状态的系统

--- database/migrations/2020_07_19_084727_edit_likes_rename_liked.php
+++ database/migrations/2020_07_19_084727_edit_likes_rename_liked.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class EditLikesRenamLiked extends Migration
+class EditLikesRenameLiked extends Migration
 {
     /**
      * Run the migrations.
@@ -14,10 +14,10 @@ class EditLikesRenamLiked extends Migration
     public function up()
     {
         Schema::table('likes', function (Blueprint $table) {
-            if (Schema::hasColumn('likes', 'liked_id')) {
+            if (Schema::hasColumn('likes', 'liked_id') && !Schema::hasColumn('likes', 'likable_id')) {
                 $table->renameColumn('liked_id', 'likable_id');
             }
-            if (Schema::hasColumn('likes', 'liked_type')) {
+            if (Schema::hasColumn('likes', 'liked_type') && !Schema::hasColumn('likes', 'likable_type')) {
                 $table->renameColumn('liked_type', 'likable_type');
             }
         });
