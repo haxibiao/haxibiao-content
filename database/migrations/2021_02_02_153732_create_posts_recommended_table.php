d4168a98ae78ff0555cdc0086978b77579bab05a
蔡志刚 <caizhigang@haxibiao.com>
2021-02-02 18:42:52 +0800

    added migration from yxsp

---
+++ database/migrations/2021_02_02_153732_create_posts_recommended_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsRecommendedTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts_recommended', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('post_id')->comment('动态ID');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts_recommended');
+    }
+}
