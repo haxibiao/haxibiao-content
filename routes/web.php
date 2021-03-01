980b027c2ff6f4858bcfae8b83c515f732757e7a
吴秀璋 <wuxiuzhang@haxibiao.com>
2021-03-01 15:36:41 +0800

    fix:注释路由Route::resource('/question'），其他question路由被这个全匹配覆盖了

--- routes/web.php
+++ routes/web.php
@@ -33,14 +33,13 @@ Route::get('/share/collection/{id}', 'CollectionController@shareCollection');
  * 问答
  */
 Route::post('/question/updateBackground', 'IssueController@add')->name('question.updateBackground');
-Route::resource('/question', 'IssueController');
+// Route::resource('/question', 'IssueController');
 Route::resource('/answer', 'SolutionController');
 Route::get('/categories-for-question', 'IssueController@categories');
 Route::get('/question-bonused', 'IssueController@bonused');
 
 //创作
 Route::middleware('auth')->get('/write', 'ArticleController@write');
-
 
 //调试和日志查看
 Route::get('/logshow', 'LogController@logShow');
