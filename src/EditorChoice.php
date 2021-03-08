c9fef1f427d94ed1068e43ae9c62cf351efd759c
zengdawei <>
2021-03-08 11:09:32 +0800

    HANJU-26 韩剧星球首页电影列表

--- src/EditorChoice.php
+++ src/EditorChoice.php
@@ -2,14 +2,26 @@
 
 namespace Haxibiao\Content;
 
+use App\Stick;
 use Haxibiao\Breeze\Model as BreezeModel;
 use Haxibiao\Breeze\User;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class EditorChoice extends BreezeModel
 {
     public function editor(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function sticks(): HasMany
+    {
+        return $this->hasMany(Stick::class);
+    }
+
+    public function resolveIndexEditorChoice($root, $args, $content, $info)
+    {
+        return self::all();
     }
 }
