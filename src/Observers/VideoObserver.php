58d8826e0c956d906aafbeaacab1a7440c9797b5
吴秀璋 <wuxiuzhang@haxibiao.com>
2021-03-18 15:54:38 +0800

    fix：采集视频没有video_id和标题

--- src/Observers/VideoObserver.php
+++ src/Observers/VideoObserver.php
@@ -3,6 +3,7 @@
 namespace Haxibiao\Content\Observers;
 
 use App\Post;
+use App\Spider;
 use Haxibiao\Media\Video;
 
 class VideoObserver
@@ -29,6 +30,18 @@ class VideoObserver
         if ($video->cover) {
             if ($post = Post::where('video_id', $video->id)->first()) {
                 Post::publishPost($post);
+            } else {
+                //支持另一种情况
+                $spider = Spider::where('spider_type', 'videos')->where('spider_id', $video->id)->first();
+                if ($spider) {
+                    if ($post = Post::where('spider_id', $spider->id)->first()) {
+                        $post->status      = Post::PUBLISH_STATUS; //发布成功动态
+                        $post->description = $spider->data['titcle'] ?? '';
+                        $post->content     = $spider->data['titcle'] ?? '';
+                        $post->video_id    = $video->id;
+                        $post->save();
+                    }
+                }
             }
         }
     }
