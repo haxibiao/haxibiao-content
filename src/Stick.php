08460efb9f25fcd5bc30ed4d2815a583b3f1fecd
wuxiuzhang <wuxiuzhang@haxibiao.com>
2021-07-08 11:43:19 +0800

    置顶place修改

--- src/Stick.php
+++ src/Stick.php
@@ -18,16 +18,16 @@ class Stick extends BreezeModel
     const CHANNEL_OF_PC  = 'WEB'; // 网站频道
 
     //APP内置顶位置
+    //前端根据place获取App置顶位置stick数组
+    //再逐一嗯就stick id获取置顶（movies｜posts｜questions）
+    //此处place表示范围较大，为App某页位置（影厅、社区、博客、合集）
     public static function getAppPlaces()
     {
         return [
-            '影厅顶部',
-            '合集顶部',
-            '每日推荐',
-            '精选美剧',
-            '精选韩剧',
-            '精选日剧',
-            '精选港剧',
+            '影厅',
+            '合集',
+            '社区',
+            '博客',
         ];
     }
 
