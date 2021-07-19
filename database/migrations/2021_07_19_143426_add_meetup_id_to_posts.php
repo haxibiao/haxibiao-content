271249de1e28752b204318b562f7a11f3494659b
蔡志刚 <caizhigang@haxibiao.com>
2021-07-19 16:20:21 +0800

    HXB-456 meetup are up!

--- database/migrations/2021_07_19_143426_add_meetup_id_to_posts.php
+++ database/migrations/2021_07_19_143426_add_meetup_id_to_posts.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddMeetUpToPosts extends Migration
+class AddMeetupIdToPosts extends Migration
 {
     /**
      * Run the migrations.
@@ -14,9 +14,12 @@ class AddMeetUpToPosts extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-			if (!Schema::hasColumn('posts', 'meet_up')) {
-				$table->boolean('meet_up')->default(false)->comment('是否关联约单');
-			}
+            if (!Schema::hasColumn('posts', 'meetup_id')) {
+                $table->unsignedInteger('meetup_id')->nullable()->comment('关联约单')->after('movie_id');
+            }
+            if (Schema::hasColumn('posts', 'meet_up')) {
+                $table->dropColumn('meet_up');
+            }
         });
     }
 
