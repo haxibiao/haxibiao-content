65bdf8f673c7ea6f9288ffd7562db9011efefe0d
蔡志刚 <caizhigang@haxibiao.com>
2021-07-29 15:29:05 +0800

    refactoring meet up

---
+++ src/Traits/Meetable.php
@@ -0,0 +1,300 @@
+<?php
+namespace Haxibiao\Content\Traits;
+
+
+use App\Chat;
+use App\Image;
+use App\OAuth;
+use App\User;
+use Haxibiao\Breeze\Exceptions\GQLException;
+use Haxibiao\Content\Article;
+
+/**
+ * Trait Meetable
+ * @package Haxibiao\Content\Traits
+ */
+trait Meetable
+{
+    public function chat(){
+        return $this->hasOne(\App\Chat::class,'article_id');
+    }
+
+    // 创建Meetup
+    public function resolveCreateMeetup($root, $args, $context, $resolveInfo)
+    {
+        $user = getUser();
+
+        $title        = data_get($args,'title');
+        $description  = data_get($args,'description');
+        $images       = data_get($args,'images');
+        $expiresAt    = data_get($args,'expires_at');
+        $expiresAt    = $expiresAt->getTimestamp();
+        $address      = data_get($args,'address');
+
+        //判断用户信息是否完整(手机号，微信)
+        static::checkUserInfo($user);
+        //检查创建约单时间不能迟于当前时间
+        static::checkExpiresAtInfo($expiresAt);
+
+
+        $article = new static();
+        $article->title     = $title;
+        $article->user_id   = $user->id;
+        $article->description = $description;
+
+        $json = [
+            'expires_at'   => $expiresAt,
+            'address'      => $address,
+            'users'        => [[
+                'id'         => $user->id,
+                'created_at' => time(),
+            ]],
+        ];
+        $article->json   = $json;
+        $article->type   = Article::MEETUP;
+        $article->status = Article::STATUS_ONLINE;
+        $article->submit = Article::SUBMITTED_SUBMIT;
+        $article->saveQuietly();
+
+        if ($images) {
+            $imageIds = [];
+            foreach ($images as $image) {
+                $model      = Image::saveImage($image);
+                $imageIds[] = $model->id;
+            }
+            $article->images()->sync($imageIds);
+        }
+        return $article;
+    }
+
+    // 加入Meetup
+    public function resolveJoinMeetup($root, $args, $context, $info)
+    {
+        $auth = getUser();
+        $meetup_id = data_get($args,'id');
+        $article = \App\Article::find($meetup_id);
+
+        //TODO  自己不能报名自己的Meetup
+
+        //检查加入约单时间，不能超过该约单的截止时间
+        $expiresAt = data_get($article,'json.expires_at');
+        throw_if(time() > $expiresAt, GQLException::class , '加入约单时间不能迟于截止时间!!');
+        $users = data_get($article,'json.users',array());
+        $uids  = data_get($users,'*.id',[]);
+
+        if(in_array($auth->id,$uids)){
+            $users = array_filter($users,function ($user)use($auth){
+                return data_get($user,'id') != $auth->id;
+            });
+            $article->forceFill([
+                'json->users'=> $users
+            ])->saveQuietly();
+            $article->joined = false;
+        } else {
+            $article->forceFill([
+                'json->users'=> array_merge($users,[[
+                    'id'         => $auth->id,
+                    'created_at' => time(),
+                ]])
+            ])->saveQuietly();
+            $article->joined = true;
+        }
+        return $article;
+    }
+
+    // Meetups
+    public function resolveMeetups($root, $args, $context, $info){
+        $perPage     = data_get($args,'first');
+        $currentPage = data_get($args,'page');
+        $filter      = data_get($args,'filter'); //TODO
+        $user_id     = data_get($args,'user_id');
+
+        $qb     = Article::whereType(Article::MEETUP)->when(!blank($user_id),function ($qb)use($user_id){
+            return $qb->where('user_id',$user_id);
+        });
+        $total  = $qb->count();
+        $meetups = $qb->skip(($currentPage * $perPage) - $perPage)
+            ->take($perPage)
+            ->get();
+        return new \Illuminate\Pagination\LengthAwarePaginator($meetups, $total, $perPage, $currentPage);
+    }
+
+    // 报名者
+    public function resolveParticipants($rootValue, $args, $context, $resolveInfo){
+        $userIds = data_get($rootValue,'json.users.*.id',[]);
+        return User::whereIn('id',$userIds);
+    }
+
+    public function resolveMeetup($root, $args, $context, $info){
+        $id      = data_get($args,'id');
+        return   Article::findOrFail($id);
+    }
+
+    // 删除Meetup
+    public function resolveDeleteMeetup($root, $args, $context, $info){
+        $id      = data_get($args,'id');
+        $article = Article::findOrFail($id);
+        throw_if($article->user_id != getUserId(), GQLException::class,'您没有删除的权限哦～～');
+        $article->delete();
+        return $article;
+    }
+
+    // 进入群聊
+    public function resolveJoinGroupChatByMeetupId($root, $args, $context, $resolveInfo){
+        $meetupId = data_get($args,'meetup_id');
+        $user     = getUser();
+        $article  = Article::findOrFail($meetupId);
+
+        $chat     = Chat::where('article_id',$meetupId)->first();
+        if(blank($chat)){
+            $uids = data_get($article,'json.users.*.id',[]);
+            $uids = array_merge([$user->id], $uids);
+            $uids = array_unique($uids);
+            sort($uids);
+            $chat = Chat::firstOrNew([
+                'article_id' => $meetupId,
+            ]);
+            $chat->subject  = $article->title;
+            $chat->uids     = $uids;
+            $chat->user_id  = $article->user_id;
+            $chat->save();
+        } else{
+            $newUids = array_merge([$user->id], $chat->uids);
+            $newUids = array_unique($newUids);
+            sort($newUids);
+            $chat->uids    = $newUids;
+            $chat->save();
+        }
+        return $chat;
+    }
+    // 更新订单
+    public function resolveUpdateMeetup($root, $args, $context, $resolveInfo)
+    {
+        // 获取用户填入的信息，录入到后台
+        $meetupId     = data_get($args,'id');
+        $title        = data_get($args,'title');
+        $description  = data_get($args,'description');
+        $images       = data_get($args,'images');
+        $expiresAt    = data_get($args,'expires_at');
+        $address      = data_get($args,'address');
+
+        //检查修改约单时间不能迟于当前时间
+        static::checkUpdateExpiresAtInfo($expiresAt);
+
+        $article = Article::findOrFail($meetupId);
+
+        //检查是否为该约单的创建者
+        throw_if($article->user_id != getUserId(), GQLException::class,'您没有修改的权限哦！！');
+
+        if(!is_null($title)){
+            $article->title = $title;
+        }
+        if(!is_null($description)){
+            $article->description = $description;
+        }
+        $json = $article->json;
+        if(!is_null($expiresAt)){
+            data_set($json,'expires_at',$expiresAt->getTimestamp());
+        }
+        if(!is_null($address)){
+            data_set($json,'address',$address);
+        }
+        $article->json = $json;
+        $article->saveQuietly();
+
+        if (!is_null($images)) {
+            $imageIds = [];
+            foreach ($images as $image) {
+                $model      = Image::saveImage($image);
+                $imageIds[] = $model->id;
+            }
+            $article->images()->sync($imageIds);
+        }
+        return $article;
+    }
+
+    // 我加入的订单
+    public function resolveJoinedMeetups($root, $args, $context, $resolveInfo){
+        $user        = getUser();
+        $perPage     = data_get($args,'first');
+        $currentPage = data_get($args,'page');
+        $status      = data_get($args,'status');
+        $qb    = Article::whereJsonContains('json->users', [['id' => $user->id]])->whereType(Article::MEETUP);
+        if(!blank($status)){
+            if($status == 'REGISTERING'){
+                $qb = $qb->where("json->expires_at",'>', now()->getTimestamp());
+            }
+            if($status == 'REGISTERED'){
+                $qb = $qb->where("json->expires_at",'<=', now()->getTimestamp());
+            }
+        }
+        $total = $qb->count();
+        $meetups = $qb->orderBy('id','desc')->skip(($currentPage * $perPage) - $perPage)
+            ->take($perPage)
+            ->get();
+        return new \Illuminate\Pagination\LengthAwarePaginator($meetups, $total, $perPage, $currentPage);
+    }
+
+    public function getRegistrationHasClosedAttribute(){
+        return \Carbon\Carbon::createFromTimestamp(data_get($this,'json.expires_at'))->isBefore(now());
+    }
+
+    public function getCountParticipantsAttribute()
+    {
+        if(!currentUser()){
+            return 0;
+        }
+        $users = data_get($this,'json.users',[]);
+        return count($users);
+    }
+
+    public function getJoinedAttribute()
+    {
+        if(!currentUser()){
+            return false;
+        }
+        $user = getUser();
+        return $this->whereJsonContains('json->users', [['id' => $user->id]])
+            ->exists();
+    }
+
+    public function getExpiresAtAttribute()
+    {
+        return \Carbon\Carbon::createFromTimestamp(data_get($this,'json.expires_at'));
+    }
+
+    public function getAddressAttribute()
+    {
+        return data_get($this,'json.address');
+    }
+
+    //限制每分钟发起约单的次数
+    private static function checkMeetupAmount($user)
+    {
+        $time = strtotime("-1 min");
+        $article = Article::where('created_at','>',date('Y-m-d H:i:s', $time))->where('user_id',$user->id)->count();
+        throw_if($article > 0 , GQLException::class, '每分钟只能发起一次约单！！');
+    }
+    //检查用户身份信息
+    private static function checkUserInfo($user)
+    {
+        $role = $user->role_id;
+        $phone = $user->phone;
+        throw_if($role != User::STAFF_ROLE && $role != User::ADMIN_STATUS , GQLException::class, '必须是员工或者管理员哦！');
+
+        $wechat = OAuth::where('user_id',$user->id)->first();
+        throw_if(!$phone || $wechat,GQLException::class,'用户信息不完整，请先补充好信息');
+    }
+    //检查创建约单时间不能迟于当前时间
+    private static function checkExpiresAtInfo($expiresAt)
+    {
+        throw_if($expiresAt < time(), GQLException::class , '约单时间不能迟于当前时间!!');
+    }
+    //检查修改约单时间不能迟于当前时间
+    private static function checkUpdateExpiresAtInfo($expiresAt)
+    {
+        throw_if($expiresAt->getTimestamp() < time(), GQLException::class , '约单时间不能迟于当前时间!!');
+    }
+
+
+}
