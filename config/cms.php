b431c1848b9631f0148e6c8106243b151e394972
ivan <ivan@mac>
2021-08-15 13:29:32 +0800

    cms 配置管理 sites_db apps_db 支持分库内容管理

--- config/cms.php
+++ config/cms.php
@@ -3,22 +3,26 @@
  * cms配置开关
  */
 return [
-    //是否APP+不同域名分库模式(一个服务器多站/多APP切换数据库实例)
-    'app_domain_switch' => [
-        // env('APP_NAME') => env('APP_DOMAIN'),
-        // 'app_name2' => '域名2'
+    //SEO域名分库模式
+    'sites_db'      => [
+        env('APP_NAME') => env('APP_DOMAIN'),
+    ],
+
+    //apps分库模式
+    'apps_db'       => [
+        env('APP_NAME') => 'gql.' . env('APP_DOMAIN'),
     ],
 
     //是否多域名站群(sites表配置)+单数据库
-    'multi_domains'     => env('MULTI_DOMAINS', false),
+    'multi_domains' => env('MULTI_DOMAINS', false),
 
     //可选主题
-    'themes'            => [
+    'themes'        => [
         'zaixianmeiju' => '在线美剧',
     ],
 
     //实名备案信息,配置到项目里，方便备案网站nova输入时选择模板
-    'icp'               => [
+    'icp'           => [
         '公司名' => [
             'copyright'          => 'Copyright ©2018-2021 公司名 All Rights Reserved',
             'record_code'        => '粤ICP备******号',
