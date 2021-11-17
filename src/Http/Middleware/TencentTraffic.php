c322bf93add306d5251f3054a85926ef2851e0f6
ivan <ivan@work>
2021-11-17 16:48:26 +0800

    腾讯的流量 不用自己的主域名，入口域名

--- src/Http/Middleware/TencentTraffic.php
+++ src/Http/Middleware/TencentTraffic.php
@@ -20,15 +20,14 @@ class TencentTraffic
             //配置了防拦截候选地址
             $redirect_urls = config('cms.tencent_traffic.redirect_urls');
             if (!empty($redirect_urls)) {
-                //随机跳转一个防拦截的h5(pwa)地址,不用自己的主域名，入口域名
+                //不用自己的主域名，入口域名 随机跳转一个防拦截的h5(pwa)地址
                 if (in_array(get_sub_domain(), [
                     config('cms.tencent_traffic.income_domain'),
                     env('APP_DOMAIN'),
                 ])) {
                     $url = array_random($redirect_urls);
+                    return redirect()->to($url);
                 }
-
-                return redirect()->to($url);
             }
         }
         return $next($request);
