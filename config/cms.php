605fbcfeae4c9965c4372b3596906596ef02a4d0
ivan <ivan@work>
2021-11-20 17:13:17 +0800

    重构enable_pwa_domains

--- config/cms.php
+++ config/cms.php
@@ -4,7 +4,7 @@
  */
 return [
     //SEO域名分库模式(顶级域名)
-    'sites'           => [
+    'sites'              => [
         // 'domain.com' => [
         //     'app_name' => 'xxyy',
         //     'db_name' => 'xxyy',
@@ -13,7 +13,7 @@ return [
     ],
 
     //apps分库模式(二级域名)
-    'apps'            => [
+    'apps'               => [
         // 'app1.domain.com' => [
         //     'app_name'    => 'xxyyapp',
         //     'db_name'    => 'xxyyapp',
@@ -22,24 +22,26 @@ return [
     ],
 
     //默认不开启cms的seo流量分析
-    'enable_traffic'  => env('ENABLE_TRAFFIC', false),
+    'enable_traffic'     => env('ENABLE_TRAFFIC', false),
 
     //针对腾讯流量的防拦截处理
-    'tencent_traffic' => [
+    'tencent_traffic'    => [
         'income_domain' => env('APP_DOMAIN'),
         'redirect_urls' => [],
     ],
 
+    'enable_pwa_domains' => [],
+
     //是否站群(原multi_domains)
-    'enable_sites'    => env('ENABLE_SITES', env('MULTI_DOMAINS', false)),
+    'enable_sites'       => env('ENABLE_SITES', env('MULTI_DOMAINS', false)),
 
     //可选主题
-    'themes'          => [
+    'themes'             => [
         'zaixianmeiju' => '在线美剧',
     ],
 
     //实名备案信息,配置到项目里，方便备案网站nova输入时选择模板
-    'icp'             => [
+    'icp'                => [
         '公司名' => [
             'copyright'          => 'Copyright ©2018-2021 公司名 All Rights Reserved',
             'record_code'        => '粤ICP备******号',
@@ -49,7 +51,7 @@ return [
     ],
 
     //站群友情链接
-    'friend_links'    => [
+    'friend_links'       => [
         // [
         //     'url'  => 'https://example2.com/',
         //     'name' => 'XX视频',
@@ -57,18 +59,18 @@ return [
     ],
 
     //matomo ids
-    'matomo_ids'      => [
+    'matomo_ids'         => [
         'domain1.com' => 'siteid1',
     ],
 
     //站群腾讯统计ids
-    'tencent_app_ids' => [
+    'tencent_app_ids'    => [
         'domain.com' => 'xxx',
     ],
 
     //百度统计ids
-    'baidu_tj_ids'    => [],
+    'baidu_tj_ids'       => [],
 
     //google统计
-    'google_tj_ids'   => [],
+    'google_tj_ids'      => [],
 ];
