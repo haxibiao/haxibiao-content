7d1071530257143acc832ffda4f151b90aea2fb2
ivan <ivan@work>
2021-11-21 10:57:08 +0800

    简单api管理缓存的当前域名的跳转地址

--- src/Http/Middleware/TencentTraffic.php
+++ src/Http/Middleware/TencentTraffic.php
@@ -17,15 +17,20 @@ class TencentTraffic
     {
         //如果是腾讯的流量
         if (isWechat() || isQQ()) {
-            //配置了防拦截候选地址
+            //配置了跳转地址
             $redirect_urls = config('cms.tencent_traffic.redirect_urls');
+            //尊重当前域名缓存的跳转地址
+            if ($cached_urls = cache()->get(get_sub_domain() . '_redirect_urls')) {
+                $redirect_urls = $cached_urls;
+            }
             if (!empty($redirect_urls)) {
-                //FIXME: 支持站群多入口域名防护被腾讯污染!!!
+                //支持站群多入口域名防护被腾讯污染!!!
                 $income_domains   = config('cms.tencent_traffic.income_domains');
                 $income_domains[] = config('cms.tencent_traffic.income_domain');
                 if (in_array(get_sub_domain(), $income_domains)) {
-                    $url = array_random($redirect_urls);
-                    return redirect()->to($url);
+                    if ($url = array_random($redirect_urls)) {
+                        return redirect()->to($url);
+                    }
                 }
             }
         }
